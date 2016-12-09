544b42921fc3366dd641af58a82d2077b9605ec0
spooktheducks <spooktheducks@gmail.com>
2016-12-08 22:24:23 -0600

    First commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+
+	"github.com/WikiLeaksFreedomForce/local-blockchain-searcher/lib/blkparser"
+)
+
+var (
+	inFile     = flag.String("infile", "", "The .dat file containing blockchain input data")
+	startBlock = flag.Int64("startBlock", 0, "The block number to start from")
+	endBlock   = flag.Int64("endBlock", 0, "The block number to end on")
+)
+
+func main() {
+	flag.Parse()
+
+	if *inFile == "" {
+		panic("Missing --infile param")
+	} else if *endBlock == 0 {
+		panic("Must specify --endBlock param")
+	}
+
+	start := uint64(*startBlock)
+	end := uint64(*endBlock)
+
+	// Set real Bitcoin network
+	magic := [4]byte{0xF9, 0xBE, 0xB4, 0xD9}
+
+	// Specify blocks directory
+	blockDB, err := blkparser.NewBlockchain(*inFile, magic, uint32(start))
+	if err != nil {
+		panic("error opening file: " + err.Error())
+	}
+
+	for i := int(start); i < int(end)+1; i++ {
+		dat, err := blockDB.FetchNextBlock()
+		if dat == nil || err != nil {
+			fmt.Println("END of DB file")
+			break
+		}
+		bl, err := blkparser.NewBlock(dat[:])
+
+		if err != nil {
+			println("Block inconsistent:", err.Error())
+			break
+		}
+
+		// Read block till we reach startBlock
+		if uint64(i) < start {
+			continue
+		}
+
+		fmt.Printf("Current block height: %v\n", i)
+		printBlock(bl)
+	}
+}
+
+func printBlock(bl *blkparser.Block) {
+	// Basic block info
+	fmt.Printf("Block hash: %v\n", bl.Hash)
+	fmt.Printf("Block time: %v\n", bl.BlockTime)
+	fmt.Printf("Block version: %v\n", bl.Version)
+	// fmt.Printf("Block parent: %v\n", btc.NewUint256(bl.ParentHash()).String())
+	fmt.Printf("Block merkle root: %v\n", bl.MerkleRoot)
+	fmt.Printf("Block bits: %v\n", bl.Bits)
+	fmt.Printf("Block size: %v\n", len(bl.Raw))
+
+	// Fetch TXs and iterate over them
+	for _, tx := range bl.Txs {
+		fmt.Printf("TxId: %v\n", tx.Hash)
+		fmt.Printf("Tx Size: %v\n", tx.Size)
+		fmt.Printf("Tx Lock time: %v\n", tx.LockTime)
+		fmt.Printf("Tx Version: %v\n", tx.Version)
+
+		fmt.Println("TxIns:")
+
+		// if tx.IsCoinBase() {
+		//  fmt.Printf("TxIn coinbase, newly generated coins")
+		// } else {
+		for txin_index, txin := range tx.TxIns {
+			fmt.Printf("TxIn index: %v\n", txin_index)
+			fmt.Printf("TxIn Input hash: %v\n", txin.InputHash)
+			fmt.Printf("TxIn Input vout: %v\n", txin.InputVout)
+			fmt.Printf("TxIn ScriptSig: %v\n", hex.EncodeToString(txin.ScriptSig))
+			fmt.Printf("TxIn Sequence: %v\n", txin.Sequence)
+		}
+		// }
+
+		fmt.Println("TxOuts:")
+
+		for txo_index, txout := range tx.TxOuts {
+			fmt.Printf("TxOut index: %v\n", txo_index)
+			fmt.Printf("TxOut value: %v\n", txout.Value)
+			fmt.Printf("TxOut script: %s\n", hex.EncodeToString(txout.Pkscript))
+			fmt.Printf("TxOut address: %v\n", txout.Addr)
+		}
+	}
+}
