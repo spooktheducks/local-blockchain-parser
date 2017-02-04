0899cf4ee76601d648dfc2f65fe79defbbd5d1b9
spooktheducks <spooktheducks@gmail.com>
2017-02-04 04:56:20 -0600

    Added OrderByValue and SkipMaxValueTxOut output formats to tx-chain

--- scanner/txdatasource/output-script.go
+++ scanner/txdatasource/output-script.go
@@ -2,12 +2,18 @@ package txdatasource
 
 import (
 	"fmt"
+	"sort"
+
+	"github.com/btcsuite/btcd/wire"
 
 	. "github.com/spooktheducks/local-blockchain-parser/blockdb"
 	"github.com/spooktheducks/local-blockchain-parser/scanner"
 )
 
-type OutputScript struct{}
+type OutputScript struct {
+	SkipMaxValueTxOut bool
+	OrderByValue      bool
+}
 
 type OutputScriptResult struct {
 	rawData []byte
@@ -21,12 +27,38 @@ var _ scanner.ITxDataSource = &OutputScript{}
 var _ scanner.ITxDataSourceResult = OutputScriptResult{}
 
 func (ds *OutputScript) Name() string {
-	return "txout-script"
+	if ds.OrderByValue && ds.SkipMaxValueTxOut {
+		return "txout-script-byvalue-skipmaxvalue"
+	} else if ds.OrderByValue {
+		return "txout-script-byvalue"
+	} else if ds.SkipMaxValueTxOut {
+		return "txout-script-skipmaxvalue"
+	} else {
+		return "txout-script"
+	}
 }
 
 func (ds *OutputScript) GetData(tx *Tx) ([]scanner.ITxDataSourceResult, error) {
+	txouts := tx.MsgTx().TxOut
+
+	skipTxoutIdx := tx.FindMaxValueTxOut()
+	if ds.OrderByValue {
+		txouts = ds.sortTxOuts(txouts)
+
+		var maxValue int64
+		for txoutIdx, txout := range txouts {
+			if txout.Value > maxValue {
+				maxValue = txout.Value
+				skipTxoutIdx = txoutIdx
+			}
+		}
+	}
+
 	results := []scanner.ITxDataSourceResult{}
-	for i := range tx.MsgTx().TxOut {
+	for i := range txouts {
+		if ds.SkipMaxValueTxOut && i == skipTxoutIdx {
+			continue
+		}
 		bs, err := tx.GetNonOPDataFromTxOut(i)
 		if err != nil {
 			continue
@@ -38,10 +70,33 @@ func (ds *OutputScript) GetData(tx *Tx) ([]scanner.ITxDataSourceResult, error) {
 	return results, nil
 }
 
+func (ds *OutputScript) sortTxOuts(txouts []*wire.TxOut) []*wire.TxOut {
+	// this copy is absolutely necessary since sort.Sort is in-place.  otherwise, later
+	// ITxDataSources will receive shuffled data.
+	txoutsCopy := make([]*wire.TxOut, len(txouts))
+	copy(txoutsCopy, txouts)
+	sort.Sort(sortableTxOuts(txoutsCopy))
+	return txoutsCopy
+}
+
 func (r OutputScriptResult) SourceName() string {
 	return fmt.Sprintf("txout-script-%d", r.index)
 }
 
 func (r OutputScriptResult) RawData() []byte {
 	return r.rawData
+}
+
+type sortableTxOuts []*wire.TxOut
+
+func (sto sortableTxOuts) Len() int {
+	return len(sto)
+}
+
+func (sto sortableTxOuts) Less(i, j int) bool {
+	return sto[i].Value < sto[j].Value
+}
+
+func (sto sortableTxOuts) Swap(i, j int) {
+	sto[i], sto[j] = sto[j], sto[i]
 }
